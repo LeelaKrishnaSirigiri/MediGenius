118d8004806a6e7f5b7c05d4a60c4be246258ff8
leelakrishna13 <sirigirileelakrishna1133@gmail.com>
2025-03-10 09:23:19 +0530

    app.py

--- Medical diagnosis using AI/app.py
+++ Medical diagnosis using AI/app.py
@@ -43,11 +43,11 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Load the saved models
 models = {
-    'diabetes': pickle.load(open('Models/diabetes_model.sav', 'rb')),
-    'heart_disease': pickle.load(open('Models/heart_disease_model.sav', 'rb')),
-    'parkinsons': pickle.load(open('Models/parkinsons_model.sav', 'rb')),
-    'lung_cancer': pickle.load(open('Models/lungs_disease_model.sav', 'rb')),
-    'thyroid': pickle.load(open('Models/Thyroid_model.sav', 'rb'))
+    'diabetes': pickle.load(open('Medical diagnosis using AI/Models/diabetes_model.sav', 'rb')),
+    'heart_disease': pickle.load(open('Medical diagnosis using AI/Models/heart_disease_model.sav', 'rb')),
+    'parkinsons': pickle.load(open('Medical diagnosis using AI/Models/parkinsons_model.sav', 'rb')),
+    'lung_cancer': pickle.load(open('Medical diagnosis using AI/Models/lungs_disease_model.sav', 'rb')),
+    'thyroid': pickle.load(open('Medical diagnosis using AI/Models/Thyroid_model.sav', 'rb'))
 }
 
 # Create a dropdown menu for disease prediction
