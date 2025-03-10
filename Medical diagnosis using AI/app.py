89a1e253a10d7f04c01f4a84c9f94d66ad7b50ef
leelakrishna13 <sirigirileelakrishna1133@gmail.com>
2025-03-10 09:32:02 +0530

    app.py

--- Medical diagnosis using AI/app.py
+++ Medical diagnosis using AI/app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pickle
 from streamlit_option_menu import option_menu
 
-# Change Name & Logo
-st.set_page_config(page_title="Disease Prediction", page_icon="⚕️")
 
-# Hiding Streamlit add-ons
+st.set_page_config(page_title="MediGenius", page_icon="🩺")
+
+
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -16,8 +16,7 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Adding Background Image
-background_image_url = "https://www.strategyand.pwc.com/m1/en/strategic-foresight/sector-strategies/healthcare/ai-powered-healthcare-solutions/img01-section1.jpg"  # Replace with your image URL
-
+background_image_url = "https://rejolut.com/wp-content/uploads/2023/08/Ai-inHealthcare-1024x683.png"
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] {{
